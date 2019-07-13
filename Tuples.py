6070f48b81c9280af53d3b5ea5b60366f499663f
Tiffany Santos <47302131+TiffanySantos@users.noreply.github.com>
2019-07-13 20:38:49 +0200

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,9 @@
+coordinates = (4, 5)
+# ()this allows you to create your tuple, NO CHANGE 
+print(coordinates[0])
+
+coordinates = [4, 5]
+# [] this creates a list which you YES CHANGE
+coordinates[0] = 10 
+print(coordinates)
+
---
+++ functions.py
@@ -0,0 +1,9 @@
+def say_hello(name, age): # create function, name it lower_case_with_underscores
+    print("Hello " + name, "you are " + str(age)) # what function does, MUST BE INTENTED
+
+print("Top")
+say_hello("Tiffany", 32) # executes the code in the function
+say_hello("Mike", 35)
+print("Bottom")
+
+# functions allow you to sandwich stuff into your code
